9b4f842e062ce221a1d5f8437c1b6bad5d311312
unknown <d.kimhuong1006@gmail.com>
2026-04-24 23:56:29 +0700

    update something

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,7 +1,8 @@
+import PageHeader from "@/components/features/PageHeader";
 export default function DashboardPage() {
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className="min-w-0 flex-1 overflow-y-auto p-8">
+            <PageHeader title="Dashboard" description="Xem tổng quan về hệ thống" />
         </div>
     );
 }
